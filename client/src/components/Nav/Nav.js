b9b6e2cf82513ebbf799828629dbcd6136ff3560
Holger Mueller <9jholger@gmail.com>
2018-07-05 15:29:57 -0500

    scaffold navbar

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nav = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">
+        </a>
+    </nav>
+)
+
+export default Nav;
