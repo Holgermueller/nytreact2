b14e0cb6f657650bc9b91e45ab6b08d9967a4d48
Holger Mueller <9jholger@gmail.com>
2018-07-05 16:14:30 -0500

    began scaffold of page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Articles from "./pages/Articles/Articles";
 
 const App = () => (
     <Router>
         <div>
             <Switch>
-                <Route exact path="/" />
-                <Route exact path="/" />
-                <Route exact path="/" />
+                <Route exact path="/" component={Articles}/>
+                <Route exact path="/articles" component={Articles}/>
+                <Route exact path="/articles/:id" component={Detail}/>
                 <Route component={NoMatch} />
             </Switch>
         </div>
---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import {link} from "react-router-dom";
+import {Col, Row, Container} from "../../components/Grid";
+import {List, ListItem} from "../../components/List";
+import {Input, TextArea, FormBtn} from "../../components/Form";
+
+
+
+export default Articles;
