d1cdfaca585c7e35bef19cda9e0ee7e1b95afc48
Holger Mueller <9jholger@gmail.com>
2018-07-05 15:07:22 -0500

    scaffold nomatch page

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,1 +1,22 @@
 import React from "react";
+import {Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../Jumbotron";
+
+const NoMatch = () => (
+    <Container fluid>
+        <Row>
+            <Col size="md-12">
+                <Jumbotron>
+                    <h1>404 Page Not Found</h1>
+                    <h1>
+                        <span role="img" aria-label="Sad Face">
+                            :(
+                        </span>
+                    </h1>
+                </Jumbotron>
+            </Col>
+        </Row>
+    </Container>
+);
+
+export default NoMatch;
