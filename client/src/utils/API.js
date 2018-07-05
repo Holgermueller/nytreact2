33146f701b28ee13576ed80c32fd63edf2e404e2
Holger Mueller <9jholger@gmail.com>
2018-07-05 14:52:57 -0500

    created react api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
 export default {
-
+	getArticles: function() {
+		return axios.get("/api/articles");
+	},
+	getArticle: function(id) {
+		return axios.get("/api/articles" + id);
+	},
+	deleteArticle: function(id){
+		return axios.delete("/api/articles" + id);
+	},
+	saveArticle: function(articleData) {
+		return axios.post("/api/articles", articleData);
+	}
 };
