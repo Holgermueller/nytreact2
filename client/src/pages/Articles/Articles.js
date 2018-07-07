9025686082c5ca668246f78a12c8c066faac5f8f
Holger Mueller <9jholger@gmail.com>
2018-07-07 10:21:14 -0500

    added some css to form

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, FormBtn } from "../../components/Form";
+import "./Article.css";
 
 class Articles extends Component {
 	state = {
