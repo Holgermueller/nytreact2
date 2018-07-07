e86c906dc57abb4a63e30133850caf0044b8af1b
Holger Mueller <9jholger@gmail.com>
2018-07-06 21:34:08 -0500

    fixed error in article.js

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import DeleteBtn from "../../components/DeleteBtn";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+import { Input, FormBtn } from "../../components/Form";
 
 class Articles extends Component {
     state = {
--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    Article: require("./articles")
+    Article: require("./article")
 };
