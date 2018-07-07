8d7a1d17edd6dd4f76ab68ed6bf78034ba1eee3f
Holger Mueller <9jholger@gmail.com>
2018-07-07 09:34:49 -0500

    fixed proxy and children errors

--- client/src/components/Grid/Col.js
+++ client/src/components/Grid/Col.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Col = ({ size, children }) => (
     <div className={size.split(" ").map(size => "col-" + size).join(" ")}>
-        [children]
+        {children}
     </div>
 );
